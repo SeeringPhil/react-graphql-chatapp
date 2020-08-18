8b44e794a778e6c6bbac45f283fa870b5530c1bc
Philippe Boulanger <philboul28@gmail.com>
2020-08-18 13:14:34 -0400

    Import shards-react components and style following tutorial: Extract message displayal to Messages component.
    Also keep the personal touch with the sent time being displayed + precise version on hover.

--- client/src/Chat.jsx
+++ client/src/Chat.jsx
@@ -4,6 +4,9 @@ import { useQuery, gql } from "@apollo/client";
 
 import { DateTime } from "luxon";
 
+import {
+    Container
+} from 'shards-react'
 
 const MESSAGES_QUERY = gql`
   query {
@@ -22,22 +25,79 @@ const client = new ApolloClient({
     cache: new InMemoryCache(),
 });
 
-const Chat = () => {
-    const { loading, error, data } = useQuery(MESSAGES_QUERY);
+
+const Messages = ({ user }) => {
+    const { loading, data, error } = useQuery(MESSAGES_QUERY);
 
     if (loading) return <p>Loading...</p>;
     if (error) return <p>Error :( </p>;
+
+    if (!data) return null
     if (data.messages.length === 0) return <p>No messages have been exchanged yet</p>
 
+    return (
+        <>
+            {data.messages.map(({ id, user: messageUser, content, at }) => (
+                <div
+                    style={{
+                        display: 'flex',
+                        justifyContent: user === messageUser ? 'flex-end' : 'flex-start',
+                        paddingTop: ".5em",
+                    }}
+                    title={DateTime.fromMillis(at).toLocaleString(DateTime.DATETIME_SHORT_WITH_SECONDS)}
+                >
 
-    return data.messages.map(({ id, user, content, at }) => (
-        <div key={id}>
-            <p>
-                {user} a écrit: <i>{content}</i>
-                <br/><b title={DateTime.fromMillis(at).toLocaleString(DateTime.DATETIME_SHORT_WITH_SECONDS)}>{DateTime.fromMillis(at).toRelative()}</b>
-            </p>
-        </div>
-    ));
+                    {user !== messageUser && (
+                        <div
+                            style={{
+                                height: 50,
+                                width: 50,
+                                marginRight: "0.5em",
+                                border: "2px solid #e5e6ea",
+                                borderRadius: 25,
+                                textAlign: "center",
+                                fontSize: "18pt",
+                                paddingTop: 5,
+                            }}
+                        >
+                            {messageUser.slice(0, 2).toUpperCase()}
+                        </div>
+
+                    )}
+
+                    <div
+                        style={{
+                            background: user === messageUser ? "#58bf56" : "#e5e6ea",
+                            color: user === messageUser ? "white" : "black",
+                            padding: '1em',
+                            borderRadius: "1em",
+                            maxWidth: "60%",
+                            paddingTop: "0.1em",
+                        }}>
+                        {content}
+
+                        <p style = {{
+                            textAlign: user === messageUser ? "right" : "left",
+                            fontSize: "8pt",
+                            marginBottom: 0,
+                            marginTop: 0
+                        }}>
+                            {DateTime.fromMillis(at).toRelative()}
+                        </p>
+
+                    </div>
+                </div>
+            ))}
+        </>
+    )
+}
+
+const Chat = () => {
+    return (
+        <Container>
+            <Messages user="Philippe" />
+        </Container>
+    )
 };
 
 export default () => (
